6db928e81cd651dc938d8af989dfb9e4eb4d1179
tingtingctt <tingtingctt@gmail.com>
2020-07-19 14:14:21 -0700

    moved public folder to root folder

---
+++ build/precache-manifest.6d67b4d3d3de2151dbbf8844d7a6ea63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fc42f3c47b1517dda9c02b22aa7eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cc1abf918a5da8fb6a6",
+    "url": "/static/js/2.4377fd07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4377fd07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5425d6c69500a48d58",
+    "url": "/static/js/main.a03498b6.chunk.js"
+  },
+  {
+    "revision": "f53ad8bb13a47484d900",
+    "url": "/static/js/runtime-main.9fd18a46.js"
+  }
+]);
