7ad61da70e43b8d7cc3c6164df9ca9524c90a90e
tingtingctt <tingtingctt@gmail.com>
2020-07-19 14:42:00 -0700

    package json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import Main from "./components/Main";
-import Wrapper from "./components/Wrapper";
-import Header from "./components/Header";
-
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blog from "./components/pages/Blog";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Wrapper>
-        <Header />
-        <Main />
-      </Wrapper>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
